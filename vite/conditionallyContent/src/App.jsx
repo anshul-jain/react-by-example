0e9ee65d3dda8a51963cfebfd47d44a42514e1b1
anshul.jain <anshul.jain@appdirect.com>
2025-02-07 21:20:06 +0530

    Conditional display rendering

--- vite/conditionallyContent/src/App.jsx
+++ vite/conditionallyContent/src/App.jsx
@@ -52,12 +52,25 @@ function App() {
   // selectedTopic : state variable to store the selected Topic value
   // setSelectedTopic : function to update the selected Topic value
   // useState('Please click a button') : initial value of the selectedTopic state variable
-  const [selectedTopic, setSelectedTopic] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState('');
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
     console.log(selectedButton);
   }
+  let tabContent = <p>Please select a topic</p>;
+  if (selectedTopic) {
+    tabContent = <div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+    </div>
+  }
+
   return (
     <div>
       <Header />
@@ -83,15 +96,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
