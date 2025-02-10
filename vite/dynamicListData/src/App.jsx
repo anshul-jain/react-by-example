fdd1dd1accc90321501627235f42623e636c9c8e
anshul.jain <anshul.jain@appdirect.com>
2025-02-10 13:26:28 +0530

    list items via spread operator

--- vite/dynamicListData/src/App.jsx
+++ vite/dynamicListData/src/App.jsx
@@ -78,14 +78,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcepts
-              image={CORE_CONCEPTS[0].image}
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-            />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcepts {...conceptItem} />)}
           </ul>
         </section>
         <section id="examples">
