0b6f5781de0a1d2e7fd492bcbb9c3cba0d1d6fa8
anshul.jain <anshul.jain@appdirect.com>
2025-02-22 19:48:28 +0530

    Core Concept changes

--- vite/forwardingProps/src/components/CoreConcept.jsx
+++ vite/forwardingProps/src/components/CoreConcept.jsx
@@ -1,13 +1,14 @@
 import CoreConcepts from './CoreConcepts' // Importing the CoreConcepts component from the CoreConcepts.jsx file
 import { CORE_CONCEPTS } from '../data'; // Importing the CORE_CONCEPTS array and Examples from the data.js file
+import Section from './Section'; // Importing the Section component from the Section.jsx file
 
 export default function CoreConcept() {
     return (
-        <section id="core-concepts">
+        <Section id="core-concepts">
             <h2>Core Concepts</h2>
             <ul>
                 {CORE_CONCEPTS.map((conceptItem) => <CoreConcepts {...conceptItem} />)}
             </ul>
-        </section>
+        </Section>
     );
 }
