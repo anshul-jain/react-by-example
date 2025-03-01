98d49053928cbe8fd2863fd56e4f2d82de97fb49
anshul.jain <anshul.jain@appdirect.com>
2025-03-01 14:42:50 +0530

    Handle text change for Name

--- vite/ticTacToe1/src/components/Player.jsx
+++ vite/ticTacToe1/src/components/Player.jsx
@@ -1,20 +1,26 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+    const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing(isEditing => !isEditing); // schedules a state update to true if false, or false if true
-        console.log('Edit button clicked');
     }
-    let playerName = <span className="player-name">{name}</span>;
+
+    function handleChange(event) {
+        console.log('handleOnChangeEdit', event.target.value);
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     if (isEditing) {
-        playerName = <input type="text" required value={name}/>;
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
     }
 
     return (
         <li>
-            {playerName}
+            {editablePlayerName}
             <span className="player-symbol">{symbol}</span>
             <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
