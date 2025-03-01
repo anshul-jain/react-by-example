85e452676fdba35b794cc4d4b4f323f886a9d6af
anshul.jain <anshul.jain@appdirect.com>
2025-03-01 13:52:36 +0530

    Click Event for Edit button

--- vite/ticTacToe1/src/components/Player.jsx
+++ vite/ticTacToe1/src/components/Player.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handleEditClick() {
+        setIsEditing(isEditing => !isEditing); // schedules a state update to true if false, or false if true
+        console.log('Edit button clicked');
+    }
+    let playerName = <span className="player-name">{name}</span>;
+    if (isEditing) {
+        playerName = <input type="text" required value={name}/>;
+    }
+
     return (
         <li>
-            <span className="player-name">{name}</span>
-            <span className="player-name">{symbol}</span>
-            <button>Edit</button>
+            {playerName}
+            <span className="player-symbol">{symbol}</span>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     );
 }
