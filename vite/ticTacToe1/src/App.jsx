1908b49d71370fa4352dd1756df23a12e41b204d
anshul.jain <anshul.jain@appdirect.com>
2025-03-04 22:08:29 +0530

    reducing the state mgmt

--- vite/ticTacToe1/src/App.jsx
+++ vite/ticTacToe1/src/App.jsx
@@ -3,18 +3,28 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = 'X';
+  if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+    currentPlayer = 'O';
+  }
+  return currentPlayer;
+}
+
 function App() {
 
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState('X');
+  const activePlayer = deriveActivePlayer(gameTurns);
+  //const [activePlayer, setActivePlayer] = useState('X');
   function handleSelectSquare(rowIndex, colIndex) {
 
-    setActivePlayer(currActivePlayer => currActivePlayer === 'X' ? 'O' : 'X');
+    //setActivePlayer(currActivePlayer => currActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns((prevTurns) => {
-      let currentPlayer = 'X';
+      /*let currentPlayer = 'X';
       if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
         currentPlayer = 'O';
-      }
+      }*/
+      const currentPlayer = deriveActivePlayer(prevTurns);
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer }, ...prevTurns];
       return updatedTurns;
@@ -32,7 +42,7 @@ function App() {
         GAME BOARD
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </menu>
   )
 }
