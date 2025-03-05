506ae85df76cbce089cb65f8fef50bc1a7cc3da5
anshul.jain <anshul.jain@appdirect.com>
2025-03-05 19:14:20 +0530

    wining combinations logic

--- vite/ticTacToe1/src/App.jsx
+++ vite/ticTacToe1/src/App.jsx
@@ -4,6 +4,12 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
+const initialBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
@@ -16,7 +22,31 @@ function App() {
 
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
+  let gameboard = initialBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameboard[row][col] = player
+
+  }
   //const [activePlayer, setActivePlayer] = useState('X');
+  let winner;
+
+  for (const combinations of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameboard[combinations[0].row][combinations[0].column];
+    const secondSquareSymbol = gameboard[combinations[1].row][combinations[1].column];
+    const thirdSquareSymbol = gameboard[combinations[2].row][combinations[2].column];
+
+    if (firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol) {
+      winner = firstSquareSymbol;
+    }
+  }
+
+
   function handleSelectSquare(rowIndex, colIndex) {
 
     //setActivePlayer(currActivePlayer => currActivePlayer === 'X' ? 'O' : 'X');
@@ -41,7 +71,8 @@ function App() {
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
         GAME BOARD
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {winner && <p>You won !! {winner}</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameboard} />
       </div>
       <Log turns={gameTurns} />
     </menu>
--- vite/ticTacToe1/src/components/GameBoard.jsx
+++ vite/ticTacToe1/src/components/GameBoard.jsx
@@ -1,12 +1,7 @@
 //import { useState } from "react";
 
-const initialBoard = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null]
-];
 // OR Array(3).fill(Array(3).fill(null)); // This will create a 2D array with 3 rows and 3 columns, each cell initialized to null.
-export default function GameBoard({ onSelectSquare, turns }) {
+export default function GameBoard({ onSelectSquare, board }) {
     /*const [gameboard, setGameboard] = useState(initialBoard);
 
     function handleSquareSelect(rowIndex, colIndex) {
@@ -20,21 +15,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
         onSelectSquare();
     } */
 
-
-    let gameboard = initialBoard;
-
-    for (const turn of turns) {
-        const { square, player } = turn;
-        const { row, col } = square;
-
-        gameboard[row][col] = player
-
-    }
-
-
     return (
         <ol id="game-board">
-            {gameboard.map((row, rowIndex) => (
+            {board.map((row, rowIndex) => (
                 <li key={rowIndex}>
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
