a52d109ac204bda5af3fecae139b82f9931574b4
anshul.jain <anshul.jain@appdirect.com>
2025-03-23 19:08:03 +0530

    Click event on the new Project button

--- vite/projectMgmtApp/src/App.jsx
+++ vite/projectMgmtApp/src/App.jsx
@@ -1,11 +1,35 @@
 import ProjectSideBar from "./components/ProjectSideBar.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import NewProject from "./components/NewProject.jsx";
+import { useState } from "react";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined, // undefined means doing noting
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectState(prevState =>{
+      return {
+        ...prevState,
+        selectedProjectId: null // null means adding new project
+      }
+    });
+  }
+
+  let content = null;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar/>
-      <NoProjectSelected/>
+      <ProjectSideBar onStartAddProject={handleStartAddProject}/>
+     {content}
     </main>
   );
 }
